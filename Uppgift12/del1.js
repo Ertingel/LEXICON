9f22eb06778efc41eab0554bfda26d23d7ecc8a7
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-01-10 14:31:45 +0100

    Done with  Del1 in Uppgift12.

---
+++ Uppgift12/del1.js
@@ -0,0 +1,57 @@
+// 1
+let age = 25
+console.log(`I am ${age} years old. `)
+
+// 2
+var foo = 2
+const bar = 5
+let baz = 7
+
+// 3
+let num = 42
+let bool = true
+let str = "str"
+let arr = []
+let obj = {}
+
+console.log(num)
+console.log(bool)
+console.log(str)
+console.log(arr)
+console.log(obj)
+
+// 4
+let name_ = "smith"
+console.log(name_)
+name_ = "john"
+
+// 5
+let is_drinking_age = age >= 18
+
+// 6
+const would_be = "appropriate"
+
+// 7
+let number1 = 3
+let number2 = 5
+
+let result0 = number1 + number2
+
+console.log(result0)
+
+// 8
+let result1 = number1 - number2
+let result2 = number1 * number2
+let result3 = number1 / number2
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
+
+// 9
+let a = 6
+console.log(a)
+
+a += 5
+// 10
+console.log(a)

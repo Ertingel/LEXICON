5b9b4fbcc2ee6cd4fd9abb177c1082685f6fa09a
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-01-11 15:18:08 +0100

    Done with Uppgift14.

--- Uppgift14/script.js
+++ Uppgift14/script.js
@@ -1,3 +1,6 @@
+// 6
+alert("Hello. ")
+
 // 1
 
 let a = prompt("What is the value of A? ")
@@ -13,3 +16,15 @@ b = parseFloat(b) || b
 let method = prompt(
 	"Out of the following '+' '-' '*' '/' would you like to do with A and B? "
 )
+
+// 4
+
+if (method === "+") alert(`The result of ${a} + ${b} is ${a + b}.`)
+else if (method === "-") alert(`The result of ${a} - ${b} is ${a - b}.`)
+else if (method === "*") alert(`The result of ${a} * ${b} is ${a * b}.`)
+else if (method === "/") alert(`The result of ${a} / ${b} is ${a / b}.`)
+// 5
+else alert(`The method of ${method} is invalid.`)
+
+// 6
+alert("Good bye. ")

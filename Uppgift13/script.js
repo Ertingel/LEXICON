f620845e5e489b26d9ea8e5594d28787e259c4cb
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-01-11 11:07:43 +0100

    Done with Uppgift13.

--- Uppgift13/script.js
+++ Uppgift13/script.js
@@ -7,11 +7,34 @@ alert(`Hello ${user_name}. `)
 let user_age = prompt("What is your birth year? ")
 // 3
 const curent_year = new Date().getFullYear()
-user_age -= curent_year
+user_age = curent_year - user_age
 
-console.log(`The age is ${user_name}. `)
-alert(`You are ${user_name} years old. `)
+console.log(`The age is ${user_age}. `)
+alert(`You are ${user_age} years old. `)
 
 // 4
-const a = prompt("What is A? ")
-const b = prompt("What is B? ")
+let a = prompt("What is A? ")
+let b = prompt("What is B? ")
+console.log(`A is ${a} and B is ${b}. `)
+
+// 5
+a = parseFloat(a) || a
+b = parseFloat(b) || b
+
+console.log(`A is ${a} and B is ${b}. `)
+
+const add = a + b
+const sub = a - b
+const mul = a * b
+const div = a / b
+
+console.log(`${a} + ${b} = ${add} 
+${a} - ${b} = ${sub} 
+${a} * ${b} = ${mul} 
+${a} / ${b} = ${div} `)
+
+// 6
+alert(`${a} + ${b} = ${add} 
+${a} - ${b} = ${sub} 
+${a} * ${b} = ${mul} 
+${a} / ${b} = ${div} `)

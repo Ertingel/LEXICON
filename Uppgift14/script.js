9c5f8e3edc2ecbbd772093482e7c6ed1007832cb
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-01-11 14:53:59 +0100

    Done with question 3 on Uppgift14.

---
+++ Uppgift14/script.js
@@ -0,0 +1,15 @@
+// 1
+
+let a = prompt("What is the value of A? ")
+a = parseFloat(a) || a
+
+// 2
+
+let b = prompt("What is the value of B? ")
+b = parseFloat(b) || b
+
+// 3
+
+let method = prompt(
+	"Out of the following '+' '-' '*' '/' would you like to do with A and B? "
+)

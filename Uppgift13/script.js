3ebd8871a3fccdb80ef6fadbead753c9f9630176
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-01-11 10:49:01 +0100

    On question 4 on Uppgift13.

---
+++ Uppgift13/script.js
@@ -0,0 +1,17 @@
+// 1
+const user_name = prompt("What is your name? ")
+console.log(`The name is ${user_name}. `)
+alert(`Hello ${user_name}. `)
+
+// 2
+let user_age = prompt("What is your birth year? ")
+// 3
+const curent_year = new Date().getFullYear()
+user_age -= curent_year
+
+console.log(`The age is ${user_name}. `)
+alert(`You are ${user_name} years old. `)
+
+// 4
+const a = prompt("What is A? ")
+const b = prompt("What is B? ")

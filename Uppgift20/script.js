383977c9b915b7dae19f411740c1955d01f52442
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-01-15 13:59:50 +0100

    Done with question 5 on Uppgift20.

---
+++ Uppgift20/script.js
@@ -0,0 +1,79 @@
+// 1
+let lexicon = {
+	name: "lexicon",
+	address: "Klarabergsviadukten 70",
+	zipcode: "Box 863, 101 37",
+	city: "STOCKHOLM",
+	students: [],
+	teachers: [],
+}
+
+// 2
+let matematik = {
+	name: "matematik",
+	students: [],
+	teacher: {},
+}
+
+let svenska = {
+	name: "svenska",
+	students: [],
+	teacher: {},
+}
+
+let historia = {
+	name: "historia",
+	students: [],
+	teacher: {},
+}
+
+// 3
+let student1 = {
+	name: "student1",
+	age: 12,
+	gender: "male",
+	subjects: [],
+}
+
+let student2 = {
+	name: "student2",
+	age: 12,
+	gender: "male",
+	subjects: [],
+}
+
+let student3 = {
+	name: "student3",
+	age: 12,
+	gender: "female",
+	subjects: [],
+}
+
+let student4 = {
+	name: "student4",
+	age: 12,
+	gender: "female",
+	subjects: [],
+}
+
+let student5 = {
+	name: "student5",
+	age: 12,
+	gender: "male",
+	subjects: [],
+}
+
+// 4
+let teacher1 = {
+	name: "teacher1",
+	subjects: [],
+}
+
+let teacher2 = {
+	name: "teacher2",
+	subjects: [],
+}
+
+// 5
+teacher1.subjects.push(matematik)
+console.log(teacher1)

e843fc70254af5df69c74eaf9eab5c7ce8eb3520
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-01-18 16:27:02 +0100

    Done with question 8 on Uppgift22.

---
+++ Uppgift22/part2.js
@@ -0,0 +1,27 @@
+// 1
+document.getElementsByClassName("logo-text")[0].style.color = "#384241"
+
+// 2
+document.querySelector("header").style.justifyContent = "flex-start"
+
+// 3
+document.querySelector("header").style.borderBottomColor = "lightgray"
+
+// 4
+document.getElementById("recipe-name").innerHTML = "Frozen Cheescake"
+
+// 5
+document
+	.getElementsByClassName("time-container")[0]
+	.children[0].classList.add("material-icons")
+
+// 6
+document.getElementsByClassName("time")[0].innerHTML = "60+ min"
+
+// 7
+document.querySelector("img").src = "./assets/frozen-cheesecake-slice.jpg"
+
+// 8
+document.getElementsByClassName(
+	"ingredients-container"
+)[0].style.backgroundColor = "#f9f9f9"

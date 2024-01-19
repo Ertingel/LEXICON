6c7758cebaaee0e7ab4ff30c9961e524039b6775
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-01-19 13:32:16 +0100

    Done with Uppgift22.

--- Uppgift22/part2.js
+++ Uppgift22/part2.js
@@ -25,3 +25,31 @@ document.querySelector("img").src = "./assets/frozen-cheesecake-slice.jpg"
 document.getElementsByClassName(
 	"ingredients-container"
 )[0].style.backgroundColor = "#f9f9f9"
+
+// 9
+document.getElementsByClassName("ingredients-list-bottom")[0].innerHTML =
+	"<li>15st digistivetex</li><li>Lite smör</li>"
+
+// 10
+document.getElementsByClassName(
+	"ingredients-list-paste"
+)[0].children[2].innerHTML = "3tsk vaniljsocker"
+
+// 11
+const ingredient = document.createElement("li")
+ingredient.innerHTML = "400g naturell philadelphiaost"
+
+document
+	.getElementsByClassName("ingredients-list-paste")[0]
+	.appendChild(ingredient)
+
+// 12
+document.querySelector("h3.instructions.shadow").classList.remove("shadow")
+
+// 13
+
+document.getElementsByClassName("instructions-list")[0].children[1].innerHTML =
+	"Separera ägggulor och äggvitor. Äggvitorna lägger du i en stor bunke, äggulorna i en liten bunke. "
+
+document.getElementsByClassName("instructions-list")[0].children[8].innerHTML =
+	"Ställ in i frysen över natten."

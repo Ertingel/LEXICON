0eebd8ddb871a0362a9b112f8e0b32dbd9bf2f23
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-01-19 16:21:05 +0100

    Done with Uppgift22.

--- Uppgift22/index.js
+++ Uppgift22/index.js
@@ -21,3 +21,33 @@ const str1 = "<div>This is a div element as a string</div>"
 
 // 6
 list.insertAdjacentHTML("afterbegin", str1)
+
+// 7
+for (i = 3; i <= 7; i++) {
+	const entry = document.createElement("p")
+	entry.innerText = `Entry ${i}`
+	list.insertAdjacentElement("beforeend", entry)
+}
+
+// 8
+console.log(list.children.length)
+
+// 9
+document.getElementsByClassName("owner")[0].innerText = "Your name"
+
+// 10
+const entry_replace = document.createElement("p")
+entry_replace.innerText = "Entry replace"
+list.insertAdjacentElement("beforeend", entry_replace)
+
+list.replaceChild(entry_replace, list.children[0])
+
+// 11
+const entry_replace2 = document.createElement("p")
+entry_replace2.innerText = "Entry replace 2"
+list.insertAdjacentElement("beforeend", entry_replace2)
+
+list.replaceChild(entry_replace2, list.children[4])
+
+// 12
+list.removeChild(list.lastChild)

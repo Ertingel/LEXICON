4264874e0a7b69f5158535035af1e629e097bdf0
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-01-22 15:27:31 +0100

    Done with question 8 on Uppgift25.

--- Uppgift25/js/script.js
+++ Uppgift25/js/script.js
@@ -1,1 +1,28 @@
 // Code here!
+
+// 1
+document.querySelector("article.art-1 > h3").innerText = "Potato"
+
+// 2
+document.querySelector("header > nav").children[0].innerText = "Start"
+
+// 3
+document.querySelector("header > nav").children[2].innerText = "Mail Us"
+
+// 4
+document.querySelector("article.art-2 > p").innerText = "Potato"
+
+// 5
+const b = document.querySelector("article.art-2 > button")
+b.style.backgroundColor = "red"
+b.innerText = "Sell"
+
+// 6
+document.querySelector("article.art-3 > figure").style.backgroundColor = "green"
+
+// 7
+document.querySelector("footer > section > article > p").innerHTML =
+	"Sinus skateboards <br> Rampsvägen 13 <br> 555 55, Railsnäs"
+
+// 8
+document.querySelectorAll("p").forEach(e => (e.style.color = "gray"))

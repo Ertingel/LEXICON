e177c1793c8dc6f9982b55ecbd9efff2541f37f6
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-01-22 16:10:46 +0100

    Done with Uppgift25.

--- Uppgift25/js/script.js
+++ Uppgift25/js/script.js
@@ -26,3 +26,41 @@ document.querySelector("footer > section > article > p").innerHTML =
 
 // 8
 document.querySelectorAll("p").forEach(e => (e.style.color = "gray"))
+
+// 9
+document.querySelectorAll("button").forEach(e => (e.innerText = "add to cart"))
+
+// 10
+document.querySelector("header > nav").children[0].classList.add("active")
+
+// 11
+const logo = document.querySelector("img.logo")
+logo.classList.remove("logo")
+
+// 12
+document
+	.querySelector("header > nav")
+	.insertAdjacentHTML("afterbegin", '<a href="#">nytt</a>')
+
+// 13
+document.querySelector("main").insertAdjacentHTML(
+	"beforeend",
+	`
+	<article class="art-4">
+		<figure><img src="img/hoodie-forrest.png" alt="hoodie"></figure>
+		<h2>Sinus Hoodie</h2>
+		<h3>Forrest</h3>
+		<p style="color: gray;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, dolores.</p>
+		<button>add to cart</button>
+	</article>
+	`
+)
+
+// 14
+logo.onclick = () => console.log("found you!")
+
+// 15
+document.querySelectorAll("main > article").forEach(e => {
+	const label = e.querySelector("h3").innerHTML
+	e.onclick = () => console.log(`Hi, Im article ${label}`)
+})

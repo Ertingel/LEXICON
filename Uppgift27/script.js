7efec7a0a18ae5be3ecb67deaa15ac7ccf6b5ca2
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-01-25 14:00:37 +0100

    Working on Uppgift27.

---
+++ Uppgift27/script.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+	const todoAdd = document.getElementById("todo-add")
+	const todoAddText = document.getElementById("todo-add-text")
+	const todoAddButton = document.getElementById("todo-add-button")
+
+	const list = document.getElementById("list")
+
+	todoAdd.onsubmit = function (e) {
+		return false
+	}
+}

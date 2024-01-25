a90c00d8a02a9f5cc6ec9b36556bfaef9cb3fe6d
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-01-25 14:59:48 +0100

    More work on Uppgift27.

--- Uppgift27/script.js
+++ Uppgift27/script.js
@@ -1,11 +1,62 @@
+function make(parent, type, { id, class: class_, ...data }) {
+	const e = document.createElement(type)
+
+	if (id) e.id = id
+	if (class_) {
+		if (typeof class_ === "string") e.classList.add(class_)
+		else e.classList.add(...class_)
+	}
+	if (data) for (const [key, value] of Object.entries(data)) e[key] = value
+	if (parent) parent.appendChild(e)
+
+	return e
+}
+
 window.onload = () => {
 	const todoAdd = document.getElementById("todo-add")
 	const todoAddText = document.getElementById("todo-add-text")
 	const todoAddButton = document.getElementById("todo-add-button")
 
-	const list = document.getElementById("list")
+	const todoList = document.getElementById("todo-list")
+
+	const addTodo = data => {
+		const item = make(todoList, "li", {
+			class: "todo",
+		})
+
+		make(item, "input", {
+			type: "checkbox",
+		})
+
+		make(item, "input", {
+			type: "button",
+			value: "-",
+		})
+
+		make(item, "input", {
+			type: "text",
+			value: data,
+		})
+
+		make(item, "input", {
+			type: "button",
+			value: "⭡",
+		})
+
+		make(item, "input", {
+			type: "button",
+			value: "⭣",
+		})
+	}
+
+	addTodo("Pizza")
 
 	todoAdd.onsubmit = function (e) {
+		if (todoAddText.value.length > 0) {
+			addTodo(todoAddText.value)
+			todoAddText.value = ""
+		}
+
 		return false
 	}
 }

d4236a668acee63d1c471675bc716928878a9bb8
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-01-29 15:30:18 +0100

    Added basic audio controll on Uppgift28.

--- Uppgift5/script.js
+++ Uppgift5/script.js
@@ -54,9 +54,63 @@ function make(parent, type, { id, class: class_, ...data }) {
 	return e
 }
 
+function timeToStr(time) {
+	return `${Math.floor((time / 60) % 60)}:${String(
+		Math.floor(time % 60)
+	).padStart(2, "0")}`
+}
+
 window.onload = () => {
 	const playlist_title = document.getElementById("playlist-title")
 	const list = document.getElementById("playlist")
+
+	const album_cover = document.getElementById("album-cover")
+	const title = document.getElementById("title")
+
+	const play_time = document.getElementById("play-time")
+	const play_progress = document.getElementById("play-progress")
+	const length = document.getElementById("length")
+
+	const play_pause = document.getElementById("play-pause")
+	const audio = document.getElementById("audio")
+
+	let playing = null
+
+	function setSong(song) {
+		if (playing) playing.classList.remove("playing")
+
+		playing = song
+		playing.classList.add("playing")
+
+		album_cover.src = playing.cover_file
+		title.innerHTML = `${playing.artist}<br /><small>${playing.song}</small>`
+		audio.src = playing.audio_file
+	}
+
+	audio.onloadedmetadata = () => {
+		play_progress.max = Math.floor(audio.duration)
+		length.innerText = timeToStr(audio.duration)
+	}
+
+	audio.ontimeupdate = () => {
+		play_time.innerText = timeToStr(audio.currentTime)
+		play_progress.value = audio.currentTime
+		play_progress.style.setProperty(
+			"--progress",
+			`${(play_progress.value / play_progress.max) * 100}%`
+		)
+	}
+
+	play_progress.oninput = () => {
+		audio.currentTime = play_progress.value
+	}
+
+	play_pause.onclick = () => {
+		if (playing) {
+			if (audio.paused) audio.play()
+			else audio.pause()
+		}
+	}
 
 	function addPlaylistSong({ artist, song, audio_file, cover_file }) {
 		const li = make(list, "li", {
@@ -65,7 +119,11 @@ window.onload = () => {
 			audio_file: `./media/${audio_file}`,
 			cover_file: `./media/${cover_file}`,
 		})
-		const button = make(li, "button", {})
+		const button = make(li, "button", {
+			onclick: () => {
+				setSong(li)
+			},
+		})
 
 		make(button, "img", { src: li.cover_file })
 		make(button, "p", {

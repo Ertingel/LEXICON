d93020e80a061ab707e62aa71cc0b81ef732b89c
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-01-30 14:03:55 +0100

    Working on Uppgift29.

--- Uppgift29/script.js
+++ Uppgift29/script.js
@@ -7,9 +7,55 @@ window.onload = () => {
 	const gueses = document.getElementById("gueses")
 	const text = document.getElementById("word")
 
-	let word = randomWord()
+	let state, word, cars, guess, current_guess
 
-	text.innerHTML = Array.from(word)
-		.map(c => `<p>${c}</p>`)
-		.join("")
+	function redraw() {
+		console.log(word, cars, guess)
+
+		const wrong = new Set()
+		const correct = new Set()
+		guess.forEach(e => {
+			if (cars.has(e)) correct.add(e)
+			else wrong.add(e)
+		})
+
+		gueses.innerHTML = Array.from(wrong)
+			.map(c =>
+				c === current_guess ? `<span class="highlight">${c}</span>` : c
+			)
+			.join(" ")
+
+		text.innerHTML = Array.from(word)
+			.map(c =>
+				correct.has(c)
+					? `<p${
+							c === current_guess ? ' class="highlight"' : ""
+					  }>${c}</p$>`
+					: "<p></p>"
+			)
+			.join("")
+	}
+
+	function newGame() {
+		word = randomWord().toUpperCase()
+		cars = new Set(word)
+		guess = new Set()
+
+		state = "play"
+
+		redraw()
+	}
+
+	document.onkeyup = e => {
+		const key = e.key.toUpperCase()
+
+		if (!/^[A-Z]$/gu.test(key)) return
+
+		current_guess = key
+		guess.add(key)
+
+		redraw()
+	}
+
+	newGame()
 }

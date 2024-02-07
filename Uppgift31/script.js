6ad011ba28e49474a288d0cd4ee24e503075cbbe
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-02-07 10:18:16 +0100

    Added character list fetching and displaying on Uppgift31.

--- Uppgift31/script.js
+++ Uppgift31/script.js
@@ -21,3 +21,42 @@ function make(parent, type, { id, class: class_, ...data }) {
 
 	return e
 }
+
+async function setPage(url = "https://swapi.dev/api/people/?page=1") {
+	const list = document.getElementById("page")
+	list.innerHTML = '<div class="loader"></div>'
+
+	const page = await (await fetch(url)).json()
+	console.log(page)
+
+	document.getElementById("prev-page").onclick = e => {
+		if (page.previous) setPage(page.previous)
+	}
+
+	document.getElementById("next-page").onclick = e => {
+		if (page.next) setPage(page.next)
+	}
+
+	const pageCount = Math.ceil(page.count / 10)
+	const pageIndex = url.match(
+		/^https:\/\/swapi\.dev\/api\/people\/\?page=(\d+)$/u
+	)[1]
+	document.getElementById(
+		"page-num"
+	).innerHTML = `${pageIndex} / ${pageCount}`
+
+	list.innerHTML = ""
+	const ul = make(list, "ul", {})
+
+	page.results.forEach(character => {
+		make(ul, "li", {
+			innerHTML: character.name,
+		})
+	})
+
+	return page
+}
+
+document.ready().then(async () => {
+	setPage()
+})

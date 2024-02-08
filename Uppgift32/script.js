80ec71bcc02011a04192e8943097463cfc824987
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-02-08 14:12:05 +0100

    Working on Uppgift32.

--- Uppgift32/script.js
+++ Uppgift32/script.js
@@ -37,25 +37,48 @@ async function memoizedFetch(url) {
 	return data
 }
 
+function viewProduct(beer) {
+	const modal = document.getElementById("modal")
+
+	const figure = make(modal, "figure", {})
+	make(figure, "img", { src: beer.image_url })
+	make(modal, "h1", { innerHTML: beer.name })
+	make(modal, "p", { innerHTML: beer.tagline })
+
+	modal.showModal()
+}
+
+let curentPage = 1
 async function viewPage(page = 1) {
 	const list = document.getElementById("list")
 
 	const data = await memoizedFetch(
-		"https://api.punkapi.com/v2/beers?page=1&per_page=24"
+		`https://api.punkapi.com/v2/beers?page=${page}&per_page=24`
 	)
 
 	data.forEach(beer => {
 		const li = make(list, "li", {})
-		make(li, "img", { src: beer.image_url })
+		const figure = make(li, "figure", {})
+		make(figure, "img", { src: beer.image_url })
 		make(li, "h1", { innerHTML: beer.name })
 		make(li, "p", { innerHTML: beer.tagline })
-	})
 
-	console.log(cache)
+		li.onclick = e => {
+			viewProduct(beer)
+		}
+	})
 }
 
 async function init() {
 	viewPage()
+
+	document.getElementById("Show More").onclick = e => {
+		e.target.style.display = "none"
+
+		viewPage(++curentPage).then(() => {
+			e.target.style.display = "block"
+		})
+	}
 }
 
 document.ready().then(async () => init())
